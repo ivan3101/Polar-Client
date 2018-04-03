0f7b0dd581e4f3796e621222a738333a8a916b58
Ivan De Menezes <ivandmenezes@gmail.com>
2018-04-03 00:32:14 -0400

    fixed issue

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -27,13 +27,14 @@ export class HeaderComponent implements OnInit, OnDestroy {
       if (this.authService.getToken() !== null) {
           this.session = true;
           this.id = this.userService.getUser()._id;
+          console.log(this.userService.getUser())
           if (this.userService.getUser().businessName) this.type = 'client';
           else this.type = 'employee';
       }
       this.sessionSubscription = this.userService.sessionEvent.subscribe(value => {
           this.session = value;
-          this.id = this.userService.getUser()._id;
-          if (this.userService.getUser().businessName) this.type = 'client';
+          this.id = JSON.parse(localStorage.getItem('user'))._id;
+          if (JSON.parse(localStorage.getItem('user')).businessName) this.type = 'client';
           else this.type = 'employee';
       });
     }
